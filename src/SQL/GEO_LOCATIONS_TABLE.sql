bf7b319feb3d4441e3c5fd3465fee9adaec08858
Rafal <rafal.juchnicki@gmail.com>
2016-10-10 22:17:24 +0200

    SQL update

--- src/SQL/GEO_LOCATIONS_TABLE.sql
+++ src/SQL/GEO_LOCATIONS_TABLE.sql
@@ -1,11 +1,11 @@
 -- Executable from main schema (SYSTEM)
 CREATE TABLE geo_locations
 (
-    geo_id INTEGER not null,
+    geo_id NUMBER(38) not null,
     geo_name VARCHAR(50),
     geo_type VARCHAR(50),
-    geo_latitude VARCHAR(50),
-    geo_longitude VARCHAR(50),
+    geo_latitude NUMBER(38),
+    geo_longitude NUMBER(38),
     CONSTRAINT geo_pk PRIMARY KEY (geo_id)
 );
 
